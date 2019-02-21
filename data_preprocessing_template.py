186de9d7b7b9277ba3abf7bf5ba944355e9d208b
Naazneen Ashfur <s10178385@connect.np.edu.sg>
2019-02-21 23:35:07 +0800

    Update data_preprocessing_template.py
    Imported libraries and datasets

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -1,14 +1,22 @@
-# Data Preprocessing Template
+# Happiness 
 
-# Importing the libraries
+#Importing libraries for data analysis
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+#Importing libraries for visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Importing the datasets
+#2015
+dataset2015 = pd.read_csv('2015.csv')
+
+#2016
+dataset2016 = pd.read_csv('2016.csv')
+
+#2017
+dataset2017 = pd.read_csv('2017.csv')
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
